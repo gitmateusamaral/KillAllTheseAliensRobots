7bdcd138a58d3f230c047812469bc3714b55c942
Mateus Amaral <reiclick123@hotmail.com>
2015-11-07 14:57:08 -0200

    Image Update

---
+++ KATAR/Assets/Scripts/SS_Behavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class SS_Behavior : MonoBehaviour 
+{
+
+	public AudioClip sound;
+	
+	void Start()
+	{
+		sound = (AudioClip)Resources.Load("sound");;	
+	}
+	
+	public void StartS(int scene)
+	{
+			Application.LoadLevel(scene);
+	}
+	
+}

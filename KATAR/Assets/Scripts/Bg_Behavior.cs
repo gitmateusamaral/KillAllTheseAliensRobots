7adbe4e655b9d57e11d090daaa09034fdbd4f15e
unknown <mauricio.silva@NAVE036-L3HNF8J.nave.local>
2015-11-16 11:34:12 -0200

    Game Update

---
+++ KATAR/Assets/Scripts/Bg_Behavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bg_Behavior : MonoBehaviour 
+{
+	public GameObject NewBg;
+	bool spawn = false;
+	void Start () 
+	{
+		this.transform.position = new Vector3(9.45f,this.transform.position.y,this.transform.position.z);
+	}
+
+	void Update () 
+	{
+		this.transform.position += new Vector3(-1*Time.deltaTime,0,0);
+		if(this.transform.position.x <= -9f && !spawn)
+		{
+			Instantiate(NewBg,new Vector3(this.transform.position.x + 35f,this.transform.position.y,this.transform.position.z),this.transform.rotation);
+			spawn = true;
+		}
+	}
+}
